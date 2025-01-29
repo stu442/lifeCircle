02bafea91ad9caceebeb7694159fde96b0eb538c
mspark <mspark@maprex.co.kr>
2025-01-29 16:53:00 +0900

    add: vercel analytics 추가 및 TODO 추가

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,10 @@
+import { Analytics } from "@vercel/analytics/react"
+
 function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="container mx-auto px-4 py-8 max-w-2xl">
       {children}
+      <Analytics />
     </div>
   );
 }
